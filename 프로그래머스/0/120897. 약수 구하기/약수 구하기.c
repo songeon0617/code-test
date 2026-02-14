d5f50f68933ab0c62adde143a23fedea86bb2b5b
songeon0617 <songeon0617@gmail.com>
2026-02-14 19:25:49 +0900

    [level 0] Title: 약수 구하기, Time: 0.06 ms, Memory: 3.91 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    
+    int count = 0;
+    
+    for(int i =1; i<=n; i++){
+        if(n%i==0) count++;
+    }
+    
+    int* answer = (int*)malloc(count*4);
+   
+    int j =0;
+    
+    for(int i =1; i<=n; i++){
+        if(n%i==0) answer[j++]=i;
+    }
+    
+    
+    
+    
+    return answer;
+}
