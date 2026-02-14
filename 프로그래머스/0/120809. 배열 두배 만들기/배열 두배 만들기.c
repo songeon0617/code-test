277af71839132d1e3a93f55b8a4dc20832d94341
songeon0617 <songeon0617@gmail.com>
2026-02-14 19:00:59 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.20 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int* solution(int numbers[], size_t numbers_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int len = numbers_len;
+    int* answer = (int*)malloc(len*sizeof(int));
+    
+    for(int i = 0; i < len; i++){
+        answer[i]=2*numbers[i];
+    }
+    return answer;
+}
