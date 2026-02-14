999ebb0131ee6048ab065f7507b56a9f4e9a71b9
songeon0617 <songeon0617@gmail.com>
2026-02-14 18:56:55 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int t) {
+    int answer = 0;
+    int count =1;
+    while (count<=t){
+        
+        
+        n*=2;
+        count++;
+       
+    }
+    answer=n;
+    return answer;
+}
