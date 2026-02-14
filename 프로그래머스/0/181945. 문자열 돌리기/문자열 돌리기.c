29e453b5ecfc58d4828aa19b25c5fc5834dbe931
songeon0617 <songeon0617@gmail.com>
2026-02-14 18:53:51 +0900

    [level 0] Title: 문자열 돌리기, Time: 2.19 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define LEN_INPUT 11
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+
+    for(int i= 0; i <sizeof(s1); i++){
+       if(s1[i]=='\0') break;
+        printf("%c\n",s1[i]); 
+        
+    }
+    
+    return 0;
+}
