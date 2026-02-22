46f7ef8afea2c78c1e9efe6548b6b4c407c586f9
songeon0617 <songeon0617@gmail.com>
2026-02-22 14:43:14 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+// str_list_len은 배열 str_list의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* str_list[], size_t str_list_len, const char* ex) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(102);
+    answer[0]='\0';
+    int j = 0;
+    for(int i =0; i< str_list_len; i++){
+        if(strstr(str_list[i],ex)!=NULL){
+            continue;
+        }
+        
+     strcat(answer, str_list[i]);
+    }
+    
+    return answer;
+}
