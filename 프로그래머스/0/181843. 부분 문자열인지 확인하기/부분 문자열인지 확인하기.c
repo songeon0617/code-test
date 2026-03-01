0761f98e5bf23e5dd5dda637ede7e005e7553f84
songeon0617 <songeon0617@gmail.com>
2026-03-01 15:36:30 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* my_string, const char* target) {
+    int answer = 0;
+    if(strstr(my_string,target)!=NULL){
+        answer = 1;
+    }
+    else answer = 0;
+    return answer;
+}
