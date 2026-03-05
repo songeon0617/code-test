681119d8000308f06f84112b116c3c56a81633da
songeon0617 <songeon0617@gmail.com>
2026-03-05 23:55:08 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(long long n) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    int s = n;
+    int count = 0;
+    while(s!=0){
+        s=s/10;
+        count++;
+        
+    }
+    int* answer = (int*)malloc(count*sizeof(int));
+    int i =0;
+    while(n!=0){
+        answer[i]=n%10;
+        n=n/10;
+        i++;
+    }
+    return answer;
+}
